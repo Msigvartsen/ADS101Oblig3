353445854511a8aa1cce9c0e370596e2bffc59a0
Magnus <magnus.sigvartsen@gmail.com>
2017-09-29 17:55:32 +0200

    Add ratingList.txt, Add nameList.txt

--- Oblig3/main.cpp
+++ Oblig3/main.cpp
@@ -2,7 +2,8 @@
 #include <hash_map>
 #include <unordered_set>
 #include "playerprofile.cpp"
-
+#include <fstream>
+#include <sstream>
 
 namespace std {
     template<>
@@ -11,7 +12,11 @@ namespace std {
     public:
         size_t operator() (const PlayerProfile& profile) const
         {
-            return profile.key % 11;
+            unsigned int hashVal{0};
+            for(auto ch : profile.key) {
+                hashVal = 37 * hashVal + ch;
+            }
+            return hashVal % 170;
         }
         bool operator() (const PlayerProfile& profile1, const PlayerProfile& profile2)
         {
@@ -20,27 +25,61 @@ namespace std {
     };
 }
 
+template<typename T>
+void readFromFile(const std::string fileName, std::vector<T> &list)
+{
+    std::ifstream read("../Oblig3/files/" + fileName +".txt");
+    if(!read) {
+        std::cerr << "Error reading from file! " << fileName << "\n";
+        std::exit(1);
+    }
+    while(read) {
+
+        T input;
+        read >> input;
+        list.push_back(input);
+    }
+    read.close();
+}
 
 int main()
 {
+    PlayerProfile player;
+    std::vector<std::string> names;
+    names.reserve(105);
+    std::vector<int> rating;
+    rating.reserve(105);
+
+    std::cout << "Names vector size: " << names.size() << "\n";
+
+    readFromFile("nameList",names);
+    readFromFile("ratingList",rating);
+
     std::unordered_set<PlayerProfile> us;
     std::unordered_set<PlayerProfile>::iterator usit;
     std::pair<std::unordered_set<PlayerProfile>::iterator,bool> par;
-    PlayerProfile magnus;
-    magnus.key = 2115;
-    magnus.nick = "Akson";
-    magnus.email = "magnus_sigvartsen@hotmail.com";
-    magnus.rating = 4310;
-    us.reserve(10);
-    us.insert(magnus);
-    magnus.key = 11;
+
+    us.reserve(170);
+    std::cout << "Load = " << us.load_factor() << "\n";
+    for(auto i{0}; i < names.size(); i++){
+        player.key = names.at(i);
+        player.rating = rating.at(i); //Needs the same size as names vector. Else it causes a crash.
+        par = us.insert(player);
+    }
 
 
-    par = us.insert(magnus);
-    usit = us.find(magnus);
-    magnus = *usit;
-    std::cout << magnus.key << magnus.nick << magnus.email << magnus.rating ;
-    //par = us.insert(magnus);
+    std::cout << "Load = " << us.load_factor() << "\n";
+    player.key = "Lyndia_Hedges";
+    usit = us.find(player);
+    player = *usit;
+    std::cout << "Searching for Lydia_Hedges: "<< player.key << " - Rating: " << player.rating << "\n";
+    player.key = "Alyson_Pollack";
+    usit = us.find(player);
+    player = *usit;
+    std::cout << "Searching for Alyson_Pollack: "<< player.key << " - Rating: " << player.rating << "\n";
+    std::cout << "Unordered_set size: " <<us.size() << "\n";
+    std::cout << "Names vector size: " << names.size() << "\n";
+    std::cout << "Load = " << us.load_factor() << "\n";
 
     return 0;
 }
--- Oblig3/playerprofile.cpp
+++ Oblig3/playerprofile.cpp
@@ -2,10 +2,11 @@
 
 
 struct PlayerProfile {
-  size_t key;
+  std::string key;
   std::string nick;
   std::string email;
   int rating;
 
   bool operator == (const PlayerProfile& t2) const { return key == t2.key; }
 };
+
